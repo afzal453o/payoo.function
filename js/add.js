326a596c5a167f37ff18d0740081a52f0cd2232c
afzal500 <hridoy50-102@diu.edu.bd>
2024-10-01 23:18:08 +0600

    added my files

---
+++ js/add.js
@@ -0,0 +1,12 @@
+document.getElementById('btn-add-money')
+.addEventListener('click',function(event){
+  event.preventDefault();
+  console.log('add money button clicked');
+//   const addMoney =document.getElementById('input-add-money')
+// const addMoney=getInputFieldValueById();
+// console.log('add money value ',addMoney);
+const addMoney=getInputFieldValueById('input-add-money');
+const pinNumber=getInputFieldValueById('input-pin-number');
+console.log('add money with parameter',addMoney,pinNumber);
+
+})
